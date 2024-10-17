5ca7b17eff4261528fa6b49f6eeb64ae5713da94
Falcon Reality <kn181000@gmail.com>
2024-10-17 23:28:17 +0530

    Removed all files except unity project

--- README.md
+++
@@ -1,6 +0,0 @@
-# care-up
-Care Up Game
-
-For quick introduction to base engine mechanics, read [Game Mechanics](Game_Mechanics.txt).
-
-For XmlTool repository, visit [XmlTool repository](https://github.com/GijsTempel/XMLTool)
--- vr-care-up/Assets/HTrackingHand.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using JetBrains.Annotations;
-using UnityEngine;
-public class HTrackingHand : MonoBehaviour
-{
-    public Transform controllerHandParentTransform;
-}
